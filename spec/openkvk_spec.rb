aa1db95b68af025bd36971159a7e2dd4e952f0cf
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2011-05-05 17:34:52 +0200

    updated tests

--- spec/openkvk_spec.rb
+++ spec/openkvk_spec.rb
@@ -4,7 +4,25 @@ describe OpenKVK do
   
   describe ".find" do
     it "should find a company" do
-      company = OpenKVK.find("Zilverline B.V.", :first)
+      response('[{"RESULT":{"TYPES":["bigint","varchar","int","int","varchar","varchar","varchar","varchar","varchar"],"HEADER":["kvk","bedrijfsnaam","kvks","sub","adres","postcode","plaats","type","website"],"ROWS":[["343774520000","Zilverline B.V.","34377452","0","Molukkenstraat 200 E4","1098TW","Amsterdam","Hoofdvestiging",null]]}}]')
+
+      company = OpenKVK.find("Zilverline", :first)
+      company.bedrijfsnaam.should == "Zilverline B.V."
+      company.kvk.should == "343774520000"
+      company.adres.should == "Molukkenstraat 200 E4"
+      company.postcode.should == "1098TW"
+      company.plaats.should == "Amsterdam"
+      company.type.should == "Hoofdvestiging"
+      company.website.should be nil
+    end
+    
+    it "should find multiple companies" do
+      response('[{"RESULT":{"TYPES":["bigint","varchar","int","int","varchar","varchar","varchar","varchar","varchar"],"HEADER":["kvk","bedrijfsnaam","kvks","sub","adres","postcode","plaats","type","website"],"ROWS":[["342838240000","Zilverline Beheer B.V.","34283824","0","Finsenstraat 56","1098RJ","Amsterdam","Hoofdvestiging",null],["343774520000","Zilverline B.V.","34377452","0","Molukkenstraat 200 E4","1098TW","Amsterdam","Hoofdvestiging",null]]}}]')
+      
+      companies = OpenKVK.find("Zilverline")
+      companies.size.should == 2
+      
+      company = companies.last
       company.bedrijfsnaam.should == "Zilverline B.V."
       company.kvk.should == "343774520000"
       company.adres.should == "Molukkenstraat 200 E4"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,11 @@ end
 
 RSpec.configure do |config|
   config.mock_with :mocha
+end
 
-  config.before(:each) do
-    # mock connection
-  end
+def response(data)
+  response = Hashie::Mash.new(:body => data)
+  Net::HTTP.stubs(:get_response).returns(response)
 end
 
 load File.expand_path('../../lib/openkvk.rb', __FILE__)
